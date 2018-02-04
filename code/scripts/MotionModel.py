8762255c52917b89fcd9f1208734977942d11ac3
Yi Yang <yiyang@Yis-MacBook-Pro.wv.cc.cmu.edu>
2018-02-03 23:07:38 -0500

    Finished motion model, but not sure if it's correct

--- code/scripts/MotionModel.py
+++ code/scripts/MotionModel.py
@@ -14,21 +14,46 @@ class MotionModel:
         """
         TODO : Initialize Motion Model parameters here
         """
+        self.alpha1 = 0.1
+        self.alpha2 = 0.1
+        self.alpha3 = 0.02
+        self.alpha4 = 0.02
 
 
     def update(self, u_t0, u_t1, x_t0):
         """
-        param[in] u_t0 : particle state odometry reading [x, y, theta] at time (t-1) [odometry_frame]   
+        param[in] u_t0 : particle state odometry reading [x, y, theta] at time (t-1) [odometry_frame]
         param[in] u_t1 : particle state odometry reading [x, y, theta] at time t [odometry_frame]
         param[in] x_t0 : particle state belief [x, y, theta] at time (t-1) [world_frame]
         param[out] x_t1 : particle state belief [x, y, theta] at time t [world_frame]
         """
 
+        delta_rot1 = math.atan2(u_t1[1] - u_t0[1], u_t1[0] - u_t0[0]) - u_t0[2]
+        delta_trans = math.sqrt(math.pow(u_t0[0] - u_t1[0], 2) + math.pow(u_t0[1] - u_t1[1], 2))
+        delta_rot2 = u_t1[2] - u_t0[2] - delta_rot1
+
+        noise_var_rot1 = self.alpha1 * math.fabs(delta_rot1) + self.alpha2 * delta_trans
+        noise_var_trans = self.alpha3 * delta_trans + self.alpha4 * (math.fabs(delta_rot1) + math.fabs(delta_rot2))
+        noise_var_rot2 = self.alpha1 * math.fabs(delta_rot2) + self.alpha2 * delta_trans
+
+        delta_rot1_hat = delta_rot1 - self.sample(noise_var_rot1)
+        delta_trans_hat = delta_trans - self.sample(noise_var_trans)
+        delta_rot2_hat = delta_rot2 - self.sample(noise_var_rot2)
+
+        x_prime = x_t0[0] + delta_trans_hat * math.cos(x_t0[2] + delta_rot1_hat)
+        y_prime = x_t0[1] + delta_trans_hat * math.sin(x_t0[2] + delta_rot1_hat)
+        theta_prime = x_t0[2] + delta_rot1_hat + delta_rot2_hat
+
+        x_t1 = [x_prime, y_prime, theta_prime]
+        return x_t1
+
+    def sample(self, noise_var):
         """
-        TODO : Add your code here
+        Sample a noise value from a given distribution parameter.
+        The value is drawn from a normal distribution with zero mean.
         """
 
-        return x_t1
+        return np.sum(noise_var * (np.random.rand(1, 12) - 0.5)) / 2
 
 if __name__=="__main__":
     pass
--- code/scripts/main.py
+++ code/scripts/main.py
@@ -28,7 +28,7 @@ def visualize_timestep(X_bar, tstep):
 def init_particles_random(num_particles, occupancy_map):
 
     # initialize [x, y, theta] positions in world_frame for all particles
-    # (randomly across the map) 
+    # (randomly across the map)
     y0_vals = np.random.uniform( 0, 7000, (num_particles, 1) )
     x0_vals = np.random.uniform( 3000, 7000, (num_particles, 1) )
     theta0_vals = np.random.uniform( -3.14, 3.14, (num_particles, 1) )
@@ -38,7 +38,7 @@ def init_particles_random(num_particles, occupancy_map):
     w0_vals = w0_vals / num_particles
 
     X_bar_init = np.hstack((x0_vals,y0_vals,theta0_vals,w0_vals))
-    
+
     return X_bar_init
 
 def init_particles_freespace(num_particles, occupancy_map):
@@ -48,15 +48,21 @@ def init_particles_freespace(num_particles, occupancy_map):
 
     """
     TODO : Add your code here
-    """ 
+    """
 
     return X_bar_init
+
+def visualize_odometry(odom):
+
+    fig = plt.figure()
+    plt.plot(odom[:, 0], odom[:, 1])
+    plt.show()
 
 def main():
 
     """
     Description of variables used
-    u_t0 : particle state odometry reading [x, y, theta] at time (t-1) [odometry_frame]   
+    u_t0 : particle state odometry reading [x, y, theta] at time (t-1) [odometry_frame]
     u_t1 : particle state odometry reading [x, y, theta] at time t [odometry_frame]
     x_t0 : particle state belief [x, y, theta] at time (t-1) [world_frame]
     x_t1 : particle state belief [x, y, theta] at time t [world_frame]
@@ -71,7 +77,7 @@ def main():
     src_path_log = '../data/log/robotdata1.log'
 
     map_obj = MapReader(src_path_map)
-    occupancy_map = map_obj.get_map() 
+    occupancy_map = map_obj.get_map()
     logfile = open(src_path_log, 'r')
 
     motion_model = MotionModel()
@@ -82,7 +88,7 @@ def main():
     X_bar = init_particles_random(num_particles, occupancy_map)
 
     vis_flag = 1
-
+    odom = np.zeros((2218, 2))
     """
     Monte Carlo Localization Algorithm : Main Loop
     """
@@ -90,6 +96,9 @@ def main():
         visualize_map(occupancy_map)
 
     first_time_idx = True
+
+    numSteps = 1
+
     for time_idx, line in enumerate(logfile):
 
         # Read a single 'line' from the log file (can be either odometry or laser measurement)
@@ -97,53 +106,59 @@ def main():
         meas_vals = np.fromstring(line[2:], dtype=np.float64, sep=' ') # convert measurement values from string to double
 
         odometry_robot = meas_vals[0:3] # odometry reading [x, y, theta] in odometry frame
+        # print odometry_robot
         time_stamp = meas_vals[-1]
 
-        # if ((time_stamp <= 0.0) | (meas_type == "O")): # ignore pure odometry measurements for now (faster debugging) 
+        # if ((time_stamp <= 0.0) | (meas_type == "O")): # ignore pure odometry measurements for now (faster debugging)
             # continue
 
         if (meas_type == "L"):
              odometry_laser = meas_vals[3:6] # [x, y, theta] coordinates of laser in odometry frame
              ranges = meas_vals[6:-1] # 180 range measurement values from single laser scan
-        
+
         print "Processing time step " + str(time_idx) + " at time " + str(time_stamp) + "s"
 
         if (first_time_idx):
             u_t0 = odometry_robot
             first_time_idx = False
+            odom[0, :] = u_t0[0:2]
             continue
 
         X_bar_new = np.zeros( (num_particles,4), dtype=np.float64)
         u_t1 = odometry_robot
-        for m in range(0, num_particles):
+        odom[numSteps, :] = u_t0[0:2]
+        numSteps += 1
+        # print u_t1
+        for m in xrange(0, num_particles):
 
             """
             MOTION MODEL
             """
             x_t0 = X_bar[m, 0:3]
             x_t1 = motion_model.update(u_t0, u_t1, x_t0)
-
             """
             SENSOR MODEL
             """
-            if (meas_type == "L"):
-                z_t = ranges
-                w_t = sensor_model.beam_range_finder_model(z_t, x_t1)
-                # w_t = 1/num_particles
-                X_bar_new[m,:] = np.hstack((x_t1, w_t))
-            else:
-                X_bar_new[m,:] = np.hstack((x_t1, X_bar[m,3]))
-        
-        X_bar = X_bar_new
+            # if (meas_type == "L"):
+            #     z_t = ranges
+            #     w_t = sensor_model.beam_range_finder_model(z_t, x_t1)
+            #     # w_t = 1/num_particles
+            #     X_bar_new[m,:] = np.hstack((x_t1, w_t))
+            # else:
+            #     X_bar_new[m,:] = np.hstack((x_t1, X_bar[m,3]))
+
+        # X_bar = X_bar_new
         u_t0 = u_t1
 
         """
         RESAMPLING
         """
-        X_bar = resampler.low_variance_sampler(X_bar)
+        # X_bar = resampler.low_variance_sampler(X_bar)
 
         if vis_flag:
             visualize_timestep(X_bar, time_idx)
+
+    # visualize_odometry(odom)
 
 if __name__=="__main__":
     main()
